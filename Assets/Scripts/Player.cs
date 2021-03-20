76ec70bc830abce8ac261674c00c68124f7753d8
andrea <andrea.alicino@gmail.com>
2021-03-20 23:53:11 +0100

    player movement clean up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMove();
+    }
+
+    void CalculateMove()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
@@ -23,24 +28,8 @@ public class Player : MonoBehaviour
 
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        //if player position on y is grater then 0 
-        //y position = 0 
-        //else if position on the y is less than -3.5f
-        //y = -3.5f
-
-        if(transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0 ,0);
-        }else if (transform.position.y <= -3.5f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.5f ,0);
-        }
-
-        //if player position on x is grater then 9 
-        //x position = 9 
-        //else if position on the x is less than -9
-        //x = -9
-
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.5f, 0),0);
+        
         if(transform.position.x >= 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y ,0);
