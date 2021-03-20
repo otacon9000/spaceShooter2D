b6e0e8558a3155a289e9570c5b082f4d78b4c3c1
andrea <andrea.alicino@gmail.com>
2021-03-20 21:58:39 +0100

    basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.0f;
+    public float horizontalInput;
+    public float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+    }
+}
