89f2b23bf02f8a11102cbb27dd2897d332e58a9f
andrea <andrea.alicino@gmail.com>
2021-03-23 11:50:17 +0100

    add enemy and enemy behaviour

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+
+    void Update()
+    {
+        Move();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Player player = other.transform.GetComponent<Player>();
+            
+            if(player != null)
+            {
+                player.Damage();
+            }
+
+            Destroy(this.gameObject);
+        }
+        if(other.CompareTag("Laser"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -6f)
+        {
+            float randomX = Random.Range(-8, 8);
+            transform.position = new Vector3(randomX, 7f, 0);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,11 +14,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _fireRate = 0.5f;
     private float _canFire = -1f;
-
-    void Start()
-    {
-    
-    }
+    [SerializeField]
+    private int _lives = 3;
 
 
     void Update()
@@ -55,5 +52,14 @@ public class Player : MonoBehaviour
         _canFire = Time.time + _fireRate;
         Instantiate(_laserPrefab, transform.position + _offset, Quaternion.identity);
         
+    }
+
+    public void Damage()
+    {
+        _lives--;
+        if(_lives < 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
