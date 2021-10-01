80463bebc1dcaa4b71a6ab7ee1a1e674152c04a5
Andrea Alicino <andrea.alicino@gmail.com>
2021-10-02 00:59:15 +0200

    enemy class and spawn manager  WIP

---
+++ Assets/Scripts/Mobile/EnemyMobile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMobile : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private int _points = 10;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 3.0f;
+    private float _canFire = 0.5f;
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    private int[] xPosSpawn = { -2, 0, 2 };
+
+
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    public virtual void  CalculateMovement()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -7f)
+        {
+            int randomX = Random.Range(0, 3);
+            transform.position = new Vector3(xPosSpawn[randomX], 9f, 0);
+        }
+    }
+
+    public virtual void FireLaser()
+    {
+        _fireRate = Random.Range(2f, 4f);
+        _canFire = Time.time + _fireRate;
+        GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
+
+        foreach (Laser element in lasers)
+        {
+            element.AssingEnemyLaser();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SpawnManagerM.Instance.enemiesInScene--;
+    }
+
+}
---
+++ Assets/Scripts/Mobile/SpawnManagerM.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerM : MonoBehaviour
+{
+    private static SpawnManagerM _instance;
+    public static SpawnManagerM Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.Log("SpawnManager is NULL");
+            }
+            return _instance;
+        }
+    }
+
+    [Header("Enemies")]
+    [SerializeField]
+    private GameObject[] _enemiesPrefab;
+    public int enemiesInScene = 0;
+
+    [Header("Power Up")]
+    [SerializeField]
+    private GameObject[] _powerUpsPrefab;
+    private bool _stopSpawn;
+
+    private int[] xPosSpawn = { -2, 0, 2 };
+
+
+
+    private void Start()
+    {
+        StartCoroutine(EnemiesSpawnRoutine());
+    }
+
+
+    IEnumerator EnemiesSpawnRoutine()
+    {
+        while (_stopSpawn == false)
+        {
+            //yield return new WaitForSeconds(Random.Range(2, 6));
+            yield return new WaitForSeconds(1f);
+            int randomX = Random.Range(0, 3);
+            Debug.Log(randomX);
+            Vector3 spawnPosition = new Vector3(xPosSpawn[randomX], 9, 0);
+            int randomEnemy = Random.Range(0, _enemiesPrefab.Length);
+            if (enemiesInScene <= 10)
+            {
+                Instantiate(_enemiesPrefab[randomEnemy], spawnPosition, Quaternion.identity);
+                enemiesInScene++;
+            }
+            //if (Random.value <= _enemiesPrefab[randomEnemy].GetComponent<PowerUp>().GetSpawnProbability())
+            //{
+            //    Instantiate(_enemiesPrefab[randomEnemy], spawnPosition, Quaternion.identity);
+            //}
+        }
+
+    }
+
+
+    IEnumerator PowerUpSpawnRoutine()
+    {
+        while (_stopSpawn == false)
+        {
+            yield return new WaitForSeconds(Random.Range(2, 6));
+            int randomX = Random.Range(0, 3);
+            Vector3 spawnPosition = new Vector3(xPosSpawn[randomX], 9, 0);
+            int randomPowerUp = Random.Range(0, _powerUpsPrefab.Length);
+
+            if (Random.value <= _powerUpsPrefab[randomPowerUp].GetComponent<PowerUp>().GetSpawnProbability())
+            {
+                Instantiate(_powerUpsPrefab[randomPowerUp], spawnPosition, Quaternion.identity);
+            }
+        }
+
+    }
+    
+    public void OnEndGame()
+    {
+        _stopSpawn = true;
+        StopAllCoroutines();
+    }
+
+}
