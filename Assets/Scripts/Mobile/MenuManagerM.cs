36d1f2319c2effde62ffe5857a053245993121ad
Andrea Alicino <andrea.alicino@gmail.com>
2021-10-02 19:17:42 +0200

    Main Menu

---
+++ Assets/Scripts/Mobile/MenuManagerM.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManagerM : MonoBehaviour
+{
+    [Header("Menu UI")]
+    [SerializeField]
+    private Text _title;
+
+    private void Start()
+    {
+        StartCoroutine(TitleFlickerRoutine());
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    IEnumerator TitleFlickerRoutine()
+    {
+        while (true)
+        {
+            _title.text = "SPACE\nDIVING";
+            yield return new WaitForSeconds(0.5f);
+            _title.text = "";
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+}
